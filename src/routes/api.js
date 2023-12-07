179bbda782148b9b6579549f1af14afdbe28fff7
natc160 <natc160@gmail.com>
2023-12-07 02:07:34 -0800

    Remove bad import

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,16 +2,15 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controller");
-const logincontroller = require("../controllers/tokenController");
 
 // GET routes
 router.get("/", controller.default);
 router.get("/user/:username", controller.user);
 
 // POST routes
-// register(username, userpassword)
+// register(username, password)
 router.post("/register", controller.register);
-// login(username, userpassword)
+// login(username, password)
 router.post("/login", controller.login);
 // home(token)
 router.post("/home", controller.home);
